6263e90af50fb510d890cc7395f401e017772ac5
Tushar Zol <tushar.zol@calsoftinc.com>
2020-08-04 18:35:43 +0530

    Auto-generated commit

---
+++ assets/mock-data.ts
@@ -0,0 +1,357 @@
+export const userList = [
+  { username: 'user', password: '123'},
+  { username: 'user1', password: '111'},
+  { username: 'user2', password: '222'},
+  { username: 'user3', password: '333'},
+  { username: 'user4', password: '444'},
+  { username: 'user5', password: '555'},
+  { username: 'user6', password: '666'},
+  { username: 'user7', password: '777'},
+  { username: 'user8', password: '888'},
+  { username: 'user9', password: '999'},
+  { username: 'user10', password: '1010'},
+  { username: 'user11', password: '1111'},
+  { username: 'user12', password: '1212'},
+  { username: 'user13', password: '1313'},
+  { username: 'user14', password: '1414'},
+  { username: 'user15', password: '1515'},
+  { username: 'user16', password: '1616'},
+  { username: 'user17', password: '1717'},
+  { username: 'user18', password: '1818'},
+  { username: 'user19', password: '1919'},
+  { username: 'user20', password: '2020'},
+  { username: 'user21', password: '2121'},
+  { username: 'user22', password: '2222'},
+  { username: 'user23', password: '2323'},
+  { username: 'user24', password: '2424'},
+  { username: 'user25', password: '2525'},
+  { username: 'user26', password: '2626'},
+  { username: 'user27', password: '2727'},
+  { username: 'user28', password: '2828'},
+  { username: 'user29', password: '2929'},
+  { username: 'user30', password: '3030'}
+  ];
+
+export const rideList = [
+    {
+      empId: 'user1',
+      vehicleType: 'bike',
+      vacantSeats: 1,
+      vehicleNo: 'MH12-AA-1234',
+      time: 10,
+      pickupPoint: 'Deccan',
+      destination: 'Hinjawadi',
+      bookedBy: ['user2', 'user3']
+    },
+    {
+      empId: 'user2',
+      vehicleType: 'car',
+      vacantSeats: 3,
+      vehicleNo: 'MH12-BB-1234',
+      time: 10.30,
+      pickupPoint: 'Magar Patta',
+      destination: 'Hinjawadi',
+      bookedBy: ['user3', 'user4']
+    },
+    {
+      empId: 'user3',
+      vehicleType: 'bike',
+      vacantSeats: 1,
+      vehicleNo: 'MH12-CC-1234',
+      time: 11,
+      pickupPoint: 'Kotharud',
+      destination: 'Hinjawadi',
+      bookedBy: ['user1', 'user5']
+    },
+    {
+      empId: 'user4',
+      vehicleType: 'car',
+      vacantSeats: 3,
+      vehicleNo: 'MH12-DD-1234',
+      time: 11.30,
+      pickupPoint: 'Vadagaon',
+      destination: 'Hinjawadi',
+      bookedBy: ['user2', 'user3']
+    },
+    {
+      empId: 'user30',
+      vehicleType: 'car',
+      vacantSeats: 3,
+      vehicleNo: 'MH12-DD-1234',
+      time: 12,
+      pickupPoint: 'Vadagaon',
+      destination: 'Hinjawadi',
+      bookedBy: ['user2', 'user3']
+    },
+    {
+        empId: 'user5',
+        vehicleType: 'bike',
+        vacantSeats: 1,
+        vehicleNo: 'MH12-DD-1234',
+        time: 12.30,
+        pickupPoint: 'Talegaon',
+        destination: 'Hinjawadi',
+        bookedBy: ['user4', 'user2']
+      },
+      {
+        empId: 'user6',
+        vehicleType: 'car',
+        vacantSeats: 3,
+        vehicleNo: 'MH12-DD-1234',
+        time: 13,
+        pickupPoint: 'Dehuroad',
+        destination: 'Hinjawadi',
+        bookedBy: ['user8', 'user7']
+      },
+      {
+        empId: 'user7',
+        vehicleType: 'bike',
+        vacantSeats: 1,
+        vehicleNo: 'MH12-DD-1234',
+        time: 13.30,
+        pickupPoint: 'Pimpri',
+        destination: 'Hinjawadi',
+        bookedBy: ['user8', 'user9']
+      },
+      {
+        empId: 'user8',
+        vehicleType: 'car',
+        vacantSeats: 3,
+        vehicleNo: 'MH12-DD-1234',
+        time: 14,
+        pickupPoint: 'Chinchwad',
+        destination: 'Hinjawadi',
+        bookedBy: ['user2', 'user3']
+      },
+      {
+        empId: 'user9',
+        vehicleType: 'bike',
+        vacantSeats: 1,
+        vehicleNo: 'MH12-DD-1234',
+        time: 14.30,
+        pickupPoint: 'Wakad',
+        destination: 'Hinjawadi',
+        bookedBy: ['user1', 'user2']
+      },
+      {
+        empId: 'user10',
+        vehicleType: 'car',
+        vacantSeats: 3,
+        vehicleNo: 'MH12-DD-1234',
+        time: 15,
+        pickupPoint: 'Wakad',
+        destination: 'Hinjawadi',
+        bookedBy: ['user8', 'user9']
+      },
+      {
+        empId: 'user11',
+        vehicleType: 'bike',
+        vacantSeats: 1,
+        vehicleNo: 'MH12-DD-1234',
+        time: 15.30,
+        pickupPoint: 'Wakad',
+        destination: 'Hinjawadi',
+        bookedBy: ['user9', 'user10']
+      },
+      {
+        empId: 'user12',
+        vehicleType: 'car',
+        vacantSeats: 3,
+        vehicleNo: 'MH12-DD-1234',
+        time: 16,
+        pickupPoint: 'Wakad',
+        destination: 'Hinjawadi',
+        bookedBy: ['user10', 'user11']
+      },
+      {
+        empId: 'user13',
+        vehicleType: 'bike',
+        vacantSeats: 1,
+        vehicleNo: 'MH12-DD-1234',
+        time: 16.30,
+        pickupPoint: 'Wakad',
+        destination: 'Hinjawadi',
+        bookedBy: ['user1', 'user2']
+      },
+      {
+        empId: 'user14',
+        vehicleType: 'bike',
+        vacantSeats: 1,
+        vehicleNo: 'MH12-DD-1234',
+        time: 17,
+        pickupPoint: 'Wakad',
+        destination: 'Hinjawadi',
+        bookedBy: ['user9', 'user10']
+      },
+      {
+        empId: 'user15',
+        vehicleType: 'car',
+        vacantSeats: 3,
+        vehicleNo: 'MH12-DD-1234',
+        time: 17.30,
+        pickupPoint: 'Wakad',
+        destination: 'Hinjawadi',
+        bookedBy: ['user11', 'user12']
+      },
+      {
+        empId: 'user16',
+        vehicleType: 'bike',
+        vacantSeats: 1,
+        vehicleNo: 'MH12-DD-1234',
+        time: 17,
+        pickupPoint: 'Wakad',
+        destination: 'Hinjawadi',
+        bookedBy: ['user9', 'user10']
+      },
+      {
+        empId: 'user17',
+        vehicleType: 'car',
+        vacantSeats: 3,
+        vehicleNo: 'MH12-DD-1234',
+        time: 17.30,
+        pickupPoint: 'Wakad',
+        destination: 'Hinjawadi',
+        bookedBy: ['user6', 'user7']
+      },
+      {
+        empId: 'user18',
+        vehicleType: 'bike',
+        vacantSeats: 1,
+        vehicleNo: 'MH12-DD-1234',
+        time: 18,
+        pickupPoint: 'Wakad',
+        destination: 'Hinjawadi',
+        bookedBy: ['user6', 'user7']
+      },
+      {
+        empId: 'user17',
+        vehicleType: 'car',
+        vacantSeats: 3,
+        vehicleNo: 'MH12-DD-1234',
+        time: 18.30,
+        pickupPoint: 'Wakad',
+        destination: 'Hinjawadi',
+        bookedBy: ['user8', 'user9']
+      },
+      {
+        empId: 'user18',
+        vehicleType: 'bike',
+        vacantSeats: 1,
+        vehicleNo: 'MH12-DD-1234',
+        time: 19,
+        pickupPoint: 'Wakad',
+        destination: 'Hinjawadi',
+        bookedBy: ['user6', 'user7']
+      },
+      {
+        empId: 'user19',
+        vehicleType: 'car',
+        vacantSeats: 3,
+        vehicleNo: 'MH12-DD-1234',
+        time: 20,
+        pickupPoint: 'Wakad',
+        destination: 'Hinjawadi',
+        bookedBy: ['user14', 'user15']
+      },
+      {
+        empId: 'user20',
+        vehicleType: 'bike',
+        vacantSeats: 1,
+        vehicleNo: 'MH12-DD-1234',
+        time: 20.30,
+        pickupPoint: 'Wakad',
+        destination: 'Hinjawadi',
+        bookedBy: ['user16', 'user17']
+      },
+      {
+        empId: 'user21',
+        vehicleType: 'car',
+        vacantSeats: 3,
+        vehicleNo: 'MH12-DD-1234',
+        time: 21,
+        pickupPoint: 'Wakad',
+        destination: 'Hinjawadi',
+        bookedBy: ['user11', 'user12']
+      },
+      {
+        empId: 'user22',
+        vehicleType: 'bike',
+        vacantSeats: 1,
+        vehicleNo: 'MH12-DD-1234',
+        time: 21.30,
+        pickupPoint: 'Wakad',
+        destination: 'Hinjawadi',
+        bookedBy: ['user19', 'user20']
+      },
+      {
+        empId: 'user23',
+        vehicleType: 'car',
+        vacantSeats: 3,
+        vehicleNo: 'MH12-DD-1234',
+        time: 22,
+        pickupPoint: 'Wakad',
+        destination: 'Hinjawadi',
+        bookedBy: ['user11', 'user20']
+      },
+      {
+        empId: 'user24',
+        vehicleType: 'bike',
+        vacantSeats: 1,
+        vehicleNo: 'MH12-DD-1234',
+        time: 22.30,
+        pickupPoint: 'Wakad',
+        destination: 'Hinjawadi',
+        bookedBy: ['user15', 'user16']
+      },
+      {
+        empId: 'user25',
+        vehicleType: 'car',
+        vacantSeats: 3,
+        vehicleNo: 'MH12-DD-1234',
+        time: 23,
+        pickupPoint: 'Wakad',
+        destination: 'Hinjawadi',
+        bookedBy: ['user11', 'user21']
+      },
+      {
+        empId: 'user26',
+        vehicleType: 'bike',
+        vacantSeats: 1,
+        vehicleNo: 'MH12-DD-1234',
+        time: 23.30,
+        pickupPoint: 'Wakad',
+        destination: 'Hinjawadi',
+        bookedBy: ['user19', 'user20']
+      },
+      {
+        empId: 'user27',
+        vehicleType: 'car',
+        vacantSeats: 3,
+        vehicleNo: 'MH12-DD-1234',
+        time: 0.0,
+        pickupPoint: 'Wakad',
+        destination: 'Hinjawadi',
+        bookedBy: ['user19', 'user20']
+      },
+      {
+        empId: 'user28',
+        vehicleType: 'bike',
+        vacantSeats: 1,
+        vehicleNo: 'MH12-DD-1234',
+        time: 0.30,
+        pickupPoint: 'Wakad',
+        destination: 'Hinjawadi',
+        bookedBy: ['user19', 'user20']
+      },
+      {
+        empId: 'user29',
+        vehicleType: 'car',
+        vacantSeats: 3,
+        vehicleNo: 'MH12-DD-1234',
+        time: 1,
+        pickupPoint: 'Wakad',
+        destination: 'Hinjawadi',
+        bookedBy: ['user1', 'user2']
+      }
+  ];
+
